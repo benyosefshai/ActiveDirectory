fac5df5bb35e77a68afc4d9056c55ca6b8bd291b
benyosefshai <32660032+benyosefshai@users.noreply.github.com>
2021-07-26 11:45:40 +1200

    Add files via upload
    User of the script enter the first name and surname of a member and prints all the AD accounts.

---
+++ Locate AD User Account.ps1
@@ -0,0 +1,9 @@
+﻿#User First Name Input
+[String] $userFirstName = Read-Host "Please enter User first name"
+
+#User Last Name Input
+[String] $userLastName = Read-Host "Please enter User last name"
+
+Write-Host "The User AD active accounts:"  (((Get-ADUser -Filter "GivenName -eq '$userFirstName' -and Surname -eq '$userLastName'")| where Enabled -eq True).Count)
+
+(Get-ADUser -Filter "GivenName -eq '$userFirstName' -and Surname -eq '$userLastName' "  |WHERE Enabled -EQ Ture  |select  samaccountname)
